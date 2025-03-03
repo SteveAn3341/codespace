844368d8f02103c5c9fe941dc82ac1f7d4e8d928
SteveAn3341 <107530990+SteveAn3341@users.noreply.github.com>
2025-03-03 04:15:27 +0000

    version7

--- practice.py
+++ practice.py
@@ -5,11 +5,15 @@
 # print(f"{greeting.title().strip( )}, {a} {b}", end="\t" , sep = "\n")
 # index = a.find("t")
 # print(a[index+1:])
+# a,b = "python" , "world"
+# print("hello, {} {}".format(a,b))
 
-a,b = "python" , "world"
-print("hello, {} {}".format(a,b))
+# print("hello,{1} {0}".format(a,b))
 
-print("hello,{1} {0}".format(a,b))
+b = "a" * 3
+print(b)
+
+
 
 
 
