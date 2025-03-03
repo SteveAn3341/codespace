024f14ed266b23d06f417eb4ddf5ca3fa739b81a
SteveAn3341 <107530990+SteveAn3341@users.noreply.github.com>
2025-03-03 04:12:33 +0000

    version5

--- practice.py
+++ practice.py
@@ -1,9 +1,18 @@
 #yunfeng AN
 #project 1
-a , b = "python" , "world"
-greeting = input("say sometihng to greet:")
-print(f"{greeting.title().strip( )}, {a} {b}")
-index = a.find("t")
-print(a[index+1:])
+# a , b = "python" , "world"
+# greeting = input("say sometihng to greet:")
+# print(f"{greeting.title().strip( )}, {a} {b}", end="\t" , sep = "\n")
+# index = a.find("t")
+# print(a[index+1:])
+
+a,b = "python" , "world"
+print("hello, {} {}".format(a,b))
+
+
+
+
+
+
 
 
